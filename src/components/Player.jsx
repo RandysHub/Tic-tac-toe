12aa3d59e0879a996b43f6f44f4cc9116c332fb5
Randyperez18 <106208354+Randyperez18@users.noreply.github.com>
2023-11-02 21:10:13 -0400

    conditionally render player name based on editing state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleClick() {
+    setIsEditing(!isEditing);
+  }
+
+  let playerName = <span className="player-name">Player1</span>;
+  if (isEditing) playerName = <input type="text" required value={name} />;
+
   return (
     <div>
       <li>
         <span className="player">
-          <span className="player-name">{name}</span>
+          {playerName}
           <span className="player-symbol">{symbol}</span>
-          <button>Edit</button>
+          <button onClick={handleClick}>Edit</button>
         </span>
       </li>
     </div>
