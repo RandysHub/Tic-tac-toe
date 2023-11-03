600fc46639dee33cda1fc48c604fbb43db5b3cf5
Randyperez18 <106208354+Randyperez18@users.noreply.github.com>
2023-11-02 20:39:44 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+function App() {
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player">
+              <span className="player-name">Player 1</span>
+              <span className="player-symbol">X</span>
+              <button>Edit</button>
+            </span>
+          </li>
+
+          <li>
+            <span className="player">
+              <span className="player-name">Player 2</span>
+              <span className="player-symbol">O</span>
+              <button>Edit</button>
+            </span>
+          </li>
+        </ol>
+      </div>
+    </main>
+  );
+}
+
+export default App;
